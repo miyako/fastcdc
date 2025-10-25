ad7dfa2e3c65ca429bdcf1bb2bd82aac07dd21f2
Keisuke Miyako <keisuke.miyako@4D.com>
2025-10-25 15:25:20 +0900

    boilerplate for darwin

---
+++ fastcdc/fastcdc.h
@@ -0,0 +1,71 @@
+#ifndef __FASTCDC_H__
+#define __FASTCDC_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <locale>
+#include <fstream>
+
+#include <json/json.h>
+#include <sstream>
+#include <iostream>
+
+#ifdef _WIN32
+#define NOMINMAX
+#define _SSIZE_T_DEFINED 1
+#include <Windows.h>
+#define snprintf _snprintf
+#define vsnprintf _vsnprintf
+#define strcasecmp _stricmp
+#define strncasecmp _strnicmp
+#define ssize_t __int64
+#endif
+
+#define BUFLEN 4096
+
+#ifdef __GNUC__
+#define _fopen fopen
+#define _fseek fseek
+#define _ftell ftell
+#define _rb "rb"
+#define _wb "wb"
+#else
+#define _fopen _wfopen
+#define _fseek _fseeki64
+#define _ftell _ftelli64
+#define _rb L"rb"
+#define _wb L"wb"
+#endif
+
+#ifdef __GNUC__
+#define OPTARG_T char*
+#include <getopt.h>
+#else
+#ifndef _WINGETOPT_H_
+#define _WINGETOPT_H_
+#define OPTARG_T wchar_t*
+#define main wmain
+#define NULL    0
+#define EOF    (-1)
+#define ERR(s, c)    if(opterr){\
+char errbuf[2];\
+errbuf[0] = c; errbuf[1] = '\n';\
+fputws(argv[0], stderr);\
+fputws(s, stderr);\
+fputwc(c, stderr);}
+#ifdef __cplusplus
+extern "C" {
+#endif
+    extern int opterr;
+    extern int optind;
+    extern int optopt;
+    extern OPTARG_T optarg;
+    extern int getopt(int argc, OPTARG_T *argv, OPTARG_T opts);
+#ifdef __cplusplus
+}
+#endif
+#endif  /* _WINGETOPT_H_ */
+#endif
+
+#endif  /* __FASTCDC_H__ */
